30542af22a6afedb7abd990e41177635ff7acd42
fathavnn <fathxvn@gmail.com>
2024-07-26 21:16:12 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,37 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function tambahTugas() {
+    if (inputBox.value === "") {
+        alert("Masukkan satu agenda!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    simpanData();
+}
+
+listContainer.addEventListener("click", function(event) {
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle("checked");
+        simpanData();
+    } else if (event.target.tagName === "SPAN") {
+        event.target.parentElement.remove();
+        simpanData();
+    }
+}, false);
+
+function simpanData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function tampilkanTugas() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+tampilkanTugas();
